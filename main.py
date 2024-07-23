fdf539ea6e40746bfe1ae77b60224c17756cc6a7
nicopoko <84250795+nikopoko@users.noreply.github.com>
2024-07-23 23:19:35 +0300

    .

--- main.py
+++ main.py
@@ -7,14 +7,34 @@ def read_text_file_to_2d_list(file_path):
             # Append the row to the matrix
             matrix.append(row)
     return matrix
-
+def output():
+    for i in range(max_y):  
+        print(massive[i])
 
 # Specify the path to your text file
 file_path = 'input.txt'
 
 # Read the text file and create the matrix
 matrix = read_text_file_to_2d_list(file_path)
-
-# Print the matrix
+massive = []
+max_x=1
+max_y=1
 for row in matrix:
-    print(row)
+    if (int(row[1])>max_x):
+        max_x=int(row[1])
+    if (int(row[2])>max_y):
+        max_y=int(row[2])
+print(max_y)
+print(max_x)
+for i in range(max_y):
+    new_massive = []
+    for j in range(max_x):
+        new_massive.append('0')
+    massive.append(new_massive)
+
+for i in matrix:
+    x = int(i[1])-1
+    y = int(i[2])-1
+    massive[y][x] = i[0]
+output()
+print(massive[16][33])
