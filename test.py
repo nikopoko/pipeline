e351e9e9e498f199b157aa4e81d80756bb5d7b7d
nicopoko <84250795+nikopoko@users.noreply.github.com>
2024-07-26 12:43:34 +0300

    .

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 # Define the function to read the file and process the input
 def read_input_file(file_path):
     items = []
@@ -97,38 +98,28 @@ def create_letter_array(items, max_y):
 def start(y,x):
     ans = 0
     if final_array[y-1][x][1]==1:
+        #print("turn up st")
         result = requrs(y-1,x,"down")
-        if result == 0:
-            final_array[y-1][x][1]=0
-        else:
+        if result != 0:
             final_array[y-1][x]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if final_array[y+1][x][0]==1:
-        
+        #print("turn down st")
         result = requrs(y+1,x,"up")
-        if result==0:
-            final_array[y+1][x][0]=0
-        else:
+        if result != 0:
             final_array[y+1][x]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if final_array[y][x+1][3]==1:
+        #print("turn right st")
         result = requrs(y,x+1,"left")
-        if result==0:
-            final_array[y][x+1][3]=0
-        else:
+        if result != 0:
             final_array[y][x+1]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if final_array[y][x-1][2]==1:
+        #print("turn left st")
         result = requrs(y,x-1,"right")
-        print("turn left")
-        if result==0:
-            final_array[y][x-1][2]=0
-        else:
+        if result != 0:
             final_array[y][x-1]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     return 0
 def requrs(y,x,direction):
@@ -136,6 +127,7 @@ def requrs(y,x,direction):
         return 0
     if final_array[y][x]==[1,1,1,1]:
         return 1
+    #print(y,x)
     up=final_array[y][x][0]
     down=final_array[y][x][1]
     right=final_array[y][x][2]
@@ -146,49 +138,34 @@ def requrs(y,x,direction):
     next_left = final_array[y][x-1][2]
     if up and next_up and direction!="up":
         result = requrs(y-1,x,"down")
-        print("turn up" ,result)
-        if result==0:
-            final_array[y-1][x]=0
-            final_array[y][x][0]=0
-        else:
+        #print("turn up")
+        if result != 0:
             final_array[y-1][x]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if down and next_down and direction!="down":
         result = requrs(y+1,x,"up")
-        print("turn down")
-        if result==0:
-            final_array[y+1][x]=0
-            final_array[y][x][1]=0
-        else:
+        #print("turn down")
+        if result != 0:
             final_array[y+1][x]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if right and next_right and direction!="right":
         result=requrs(y,x+1,"left")
-        print("turn right")
-        if result==0:
-            final_array[y][x+1]=0
-            final_array[y][x][2]=0
-        else:
+        #print("turn right")
+        if result != 0:
             final_array[y][x+1]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
     if left and next_left and direction!="left":
         result=requrs(y,x-1,"right")
-        print("turn left")
-        if result==0:
-            final_array[y][x-1]=0
-            final_array[y][x][3]=0
-        else:
+        #print("turn left en")
+        if result != 0:
             final_array[y][x-1]=[1,1,1,1]
-            final_array[y][x]=[1,1,1,1]
             return 1
+        
     return 0
 
 
 # Define the file path
-file_path = 'test.txt'
+file_path = 'input.txt'
 
 # Read and process the input file
 items = read_input_file(file_path)
@@ -211,8 +188,7 @@ for row in final_array:
     print(row)
 # Print the array with name, coordinates, and letter
 print("\nArray with Names, Coordinates, and Letters:")
-
-
+print(stars)
 for item in letter_array:
     print(item)
-    print(start(item['y'],item['x']))
+    #print(start(item['y'],item['x']))
